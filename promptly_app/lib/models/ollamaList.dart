29ad26a1b4eadceea7407af39e951e328a25b92b
thenry <thenry@student.42.fr>
2024-11-26 21:45:14 +0100

    truncate ollama models name for readability

--- promptly_app/lib/models/ollamaList.dart
+++ promptly_app/lib/models/ollamaList.dart
@@ -7,13 +7,20 @@ Future<List<Map<String, String>>> getOllamaModels() async {
   try {
     final client = OllamaClient();
     final response = await client.listModels();
-    
+
     // Convert to List of Maps with name and type
     final List<Map<String, String>> modelList = response.models!
-        .map((model) => {
-          'name': model.model.toString(),  // Changed from toString() to name
-          'type': 'local',
-        })
+        .map((model) {
+          String modelName = model.model.toString();
+          if (modelName.contains(':')) {
+            modelName = modelName.substring(0, modelName.indexOf(':'));
+          }
+          
+          return {
+            'name': modelName,
+            'type': 'local',
+          };
+        }).cast<Map<String, String>>()
         .toList();
     
     // Clear existing list and add new models
@@ -28,3 +35,4 @@ Future<List<Map<String, String>>> getOllamaModels() async {
     return llmList;
   }
 }
+
