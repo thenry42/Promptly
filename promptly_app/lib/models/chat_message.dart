17bfbee7dfefb3fe2bd23921b6b461fd80cdee3b
thenry <thenry@student.42.fr>
2024-11-28 18:38:51 +0100

    again

---
+++ promptly_app/lib/models/chat_message.dart
@@ -0,0 +1,12 @@
+/* ChatMessage class that represent a single chat message
+It contains two properties:
+  - [sender]: The name or identifier of the person sending the message.
+  - [message]: The content of the message sent by the sender. */
+
+class ChatMessage {
+
+  final String sender;
+  final String message;
+
+  ChatMessage({required this.sender, required this.message});
+}
---
+++ promptly_app/lib/widgets/chat_message_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:promptly_app/widgets/colors.dart';
+import '../models/chat_message.dart';
+
+class ChatMessageWidget extends StatelessWidget {
+  final ChatMessage message;
+
+  const ChatMessageWidget({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: message.sender == 'User' ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: message.sender == 'User' ? AppColors.powderblue : AppColors.lightpink,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(message.message),
+      ),
+    );
+  }
+}
