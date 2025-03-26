2623de958406b78869fd0c3545ab977caf77265c
tibzer <tibzer@device-36.home>
2025-03-26 19:22:18 +0100

    time for a new rework

---
+++ promptly_app/lib/srcs/backend/Deepseek.dart
@@ -0,0 +1,13 @@
+import 'package:deepseek/deepseek.dart' as deepseek;
+import 'package:flutter/foundation.dart';
+import 'package:promptly_app/srcs/backend/ChatMessage.dart';
+import 'package:promptly_app/srcs/backend/Singleton.dart';
+
+class Deepseek
+{
+  // ATTRIBUTES -------------------------------------------
+
+  // CONSTRUCTOR ------------------------------------------
+
+  // METHODS ----------------------------------------------
+}
---
+++ promptly_app/lib/srcs/backend/Gemini.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:google_generative_ai/google_generative_ai.dart' as google;
+import 'ChatMessage.dart';
+import 'Singleton.dart';
+
+class Gemini
+{
+  // ATTRIBUTES -------------------------------------------
+
+  // CONSTRUCTOR ------------------------------------------
+
+  // METHODS ----------------------------------------------
+}
